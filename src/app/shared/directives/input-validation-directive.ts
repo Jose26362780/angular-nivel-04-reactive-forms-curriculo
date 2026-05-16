7e105491f3eedb2e2f1aa612ea8c85dfd5a54f20
Jose Martinez <juniorjose1925@gmail.com>
2026-05-16 18:52:50 -0300

    feat: tornar diretiva de validação reativa ao status do formulário

--- src/app/shared/directives/input-validation-directive.ts
+++ src/app/shared/directives/input-validation-directive.ts
@@ -1,17 +1,46 @@
-import { Directive, HostListener, Input } from '@angular/core';
+import { DestroyRef, Directive, HostListener, inject, Input } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { NgControl } from '@angular/forms';
 
 @Directive({
   selector: '[appInputValidation]',
 })
 export class InputValidationDirective {
+  private readonly _ngControl = inject(NgControl, {
+    self: true,
+  });
+
+  private readonly _destroyRef = inject(DestroyRef);
+
   @Input() errorMessage = 'Campo Invalido';
 
   ngOnInit() {
     console.log('OnInit errorMessage: ', this.errorMessage);
+
+    this._ngControl.statusChanges?.pipe(takeUntilDestroyed(this._destroyRef)).subscribe(() => {
+      console.log('Status Mudou!');
+
+      this.updateStatus();
+    });
   }
 
   @HostListener('blur')
   onBlur() {
     console.log('Ocorreu um blur');
+    this.updateStatus();
+  }
+
+  updateStatus() {
+    const control = this._ngControl.control;
+
+    if (!control) return;
+
+    const isInvalid = control.invalid && control.touched;
+
+    if (isInvalid) {
+      console.log('Esta invalido');
+    } else {
+      console.log('Esta valido');
+    }
   }
 }
